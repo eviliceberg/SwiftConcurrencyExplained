f0d6cc11720646b8f5c8b9a2f1b8f94650c03b63
evilicebergs <voiced.fair-0a@icloud.com>
2025-04-22 13:07:40 +0300

    Initial Commit

---
+++ SwiftConcurrency/SwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyApp.swift
+//  SwiftConcurrency
+//
+//  Created by Artem Golovchenko on 2025-04-22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
