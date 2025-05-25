eca47645f7aa315292f3f2253043d9a3154dabc4
evilicebergs <voiced.fair-0a@icloud.com>
2025-05-25 14:53:18 +0300

    creating custom shapes

---
+++ SwiftConcurrency/CustomShapes.swift
@@ -0,0 +1,68 @@
+//
+//  CustomShapes.swift
+//  SwiftConcurrency
+//
+//  Created by Artem Golovchenko on 2025-05-25.
+//
+
+import SwiftUI
+
+struct Triangle: Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        }
+    }
+}
+
+struct Diamond: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            let horizontalOffset: CGFloat = rect.width * 0.2
+            
+            
+            path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX - horizontalOffset, y: rect.midY))
+            path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX + horizontalOffset, y: rect.midY))
+            path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        }
+    }
+    
+    
+}
+
+struct Trapezoid: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            let horizontalOffset: CGFloat = rect.width * 0.2
+            
+            path.move(to: CGPoint(x: rect.minX + horizontalOffset, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX - horizontalOffset, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX + horizontalOffset, y: rect.minY))
+        }
+    }
+}
+
+struct CustomShapes: View {
+    var body: some View {
+        ZStack {
+            Trapezoid()
+            //Triangle()
+           //     .stroke(lineWidth: 3)
+                //.trim(from: 0, to: 0.5)
+//                .fill(LinearGradient(gradient: Gradient(colors: [.red, .blue]), startPoint: .topLeading, endPoint: .bottomTrailing))
+                .frame(width: 300, height: 100)
+        }
+    }
+}
+
+#Preview {
+    CustomShapes()
+}
